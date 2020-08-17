65de4d4541aa4b4faab2e3910f9c4bbfc6b2f3b0
Guy Cherkesky <guy@cherkesky.com>
2020-08-17 11:53:04 -0500

    corrected csv from extra info

--- todo.ex
+++ todo.ex
@@ -4,10 +4,11 @@ defmodule Todo do
     #parse the data
     #ask user for command
     #read todos, add todos, delete todos, load file, save file
+
   def start do
     filename = IO.gets("Name of the CSV to load: ")
               |> String.trim
-
+    read(filename)
   end
 
   def read(filename) do
@@ -15,9 +16,13 @@ defmodule Todo do
       {:ok, body} -> body
       {:error, reason} -> IO.puts ~s(Could not open file "#{filename}"\n)
                           IO.inspect(reason)
+                          start()
     end
   end
 
+  def parse(body) do
+    lines = String.split(body, ~r{(\r\n|\r|\n)})
+  end
 
 
 end
